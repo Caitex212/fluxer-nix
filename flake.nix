782cde2f4ca683cf618d3f1eb85e05187c9cfa17
Caitex212 <115795867+Caitex212@users.noreply.github.com>
2026-06-15 13:41:15 +0200

    Refactor flake.nix to include system and pkgs

--- flake.nix
+++ flake.nix
@@ -1,16 +1,18 @@
 {
   description = "Fluxer canary NixOS overlay";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
-  outputs = { self, nixpkgs, ... }: {
-    overlays.default = final: prev: {
-      fluxer-canary = prev.appimageTools.wrapType2 {
+  outputs = { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+    in {
+      packages.${system}.fluxer-canary = pkgs.appimageTools.wrapType2 {
         pname = "fluxer-canary";
         version = "2026.602.31138";
-        src = prev.fetchurl {
+        src = pkgs.fetchurl {
           url = "https://api.canary.fluxer.app/dl/desktop/canary/linux/x64/2026.602.31138/appimage";
           sha256 = "0dmfzd3x652w5dr3h39bkzlxvq3vr3lmyg2sxslqrjnn19dl10bp";
         };
       };
     };
-  };
 }
